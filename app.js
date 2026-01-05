645ac109375a14dd54373b9ff2b3259453a509cb
emerson Gabriel Martins <emerson Gabriel Martins>
2026-01-05 09:55:58 -0300

    Ajuste do servidor para o Render

--- app.js
+++ app.js
@@ -4,24 +4,22 @@ const path = require('path');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// 1. Configura o servidor para servir arquivos da pasta 'public'
-// Isso faz com que o CSS, imagens e HTMLs sejam encontrados automaticamente
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 2. Rota Principal (Opcional)
-// Se alguém acessar apenas 'seusite.onrender.com', será enviado para a mesa 1
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'mesa.html'));
 });
 
-// 3. Lógica do Socket.io (Comunicação em tempo real)
+
 io.on('connection', (socket) => {
     console.log('Um usuário se conectou');
 
-    // Escuta quando uma mesa envia um pedido
+   
     socket.on('fazer_pedido', (dados) => {
         console.log('Pedido recebido:', dados);
-        // Repassa o pedido para o painel da cozinha
+        
         io.emit('receber_pedido', dados);
     });
 
